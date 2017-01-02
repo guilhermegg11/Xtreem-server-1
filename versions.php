ce255a0ae0e442ddafb4b01df7fca34c26190cba
Xtreem <guilhermegg11@hotmail.com>
2017-01-02 16:33:33 -0300

    Add files via upload

---
+++ versions.php
@@ -0,0 +1,33 @@
+<table border="1px" cellspacing="0px" cellpadding="1px"><tr><th>Client</th><th title="Count">n</th><th title="Percent of total">%</th></tr><tr><td>TMW/0.0.29.1
+</td><td>332</td><td>49.11</td></tr><tr><td>ManaPlus (Linux; mana; 4144 v1.1.1.16)
+</td><td>146</td><td>21.6</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.1.1.16)
+</td><td>82</td><td>12.13</td></tr><tr><td>ManaPlus (Linux; mana; 4144 v1.0.10.5)
+</td><td>13</td><td>1.92</td></tr><tr><td>TMW/0.0.24.1
+</td><td>12</td><td>1.78</td></tr><tr><td>Mana/0.5.0 (Linux; mana)
+
+</td><td>10</td><td>1.48</td></tr><tr><td>ManaPlus (Linux; mana; 4144 v1.1.1.9)
+</td><td>9</td><td>1.33</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.0.12.26)
+</td><td>9</td><td>1.33</td></tr><tr><td>TMW/0.0.25
+</td><td>7</td><td>1.04</td></tr><tr><td>Mana/1.0-git (Windows; mana; 4144 v1.0.4.14)
+</td><td>6</td><td>0.89</td></tr><tr><td>Mana/1.0.0
+</td><td>6</td><td>0.89</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.1.1.9)
+
+</td><td>6</td><td>0.89</td></tr><tr><td>Mana/0.5.0 (Apple; mana)
+</td><td>5</td><td>0.74</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.0.9.19)
+</td><td>5</td><td>0.74</td></tr><tr><td>TMW/0.0.28.wsm
+</td><td>3</td><td>0.44</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.0.11.28)
+</td><td>3</td><td>0.44</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.0.11.14)
+</td><td>3</td><td>0.44</td></tr><tr><td>Mana/0.5.0 (Apple; tmw)
+
+</td><td>3</td><td>0.44</td></tr><tr><td>Mana/1.0.0 (Linux; )
+</td><td>2</td><td>0.3</td></tr><tr><td>Mana/1.0.0-beta2 (Linux; mana)
+</td><td>2</td><td>0.3</td></tr><tr><td>TMW/0.0.28.1
+</td><td>2</td><td>0.3</td></tr><tr><td>ManaPlus (Linux; mana; 4144 v1.0.10.31)
+</td><td>2</td><td>0.3</td></tr><tr><td>ManaPlus (Linux; mana; 4144 v1.0.11.14)
+</td><td>2</td><td>0.3</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.0.12.12)
+
+</td><td>2</td><td>0.3</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.0.10.4)
+</td><td>1</td><td>0.15</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.0.8.3-17.08.10)
+</td><td>1</td><td>0.15</td></tr><tr><td>ManaPlus (Linux; mana; 4144 v1.0.12.26)
+</td><td>1</td><td>0.15</td></tr><tr><td>ManaPlus (Windows; mana; 4144 v1.0.10.31)
+</td><td>1</td><td>0.15</td></tr></table><br />Total number of connections: 676<br />Number of different clients reported: 28<br /><br />Data retrieved: Fri Jan 28 11, 2:47 pm CET
